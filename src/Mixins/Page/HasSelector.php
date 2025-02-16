28779908915df0eff947499844bc7451ef5924d2
ArefShojaei <arefshojaei82dev@gmail.com>
2025-02-16 15:43:09 +0330

    ADD: add HasSelector trait

---
+++ src/Mixins/Page/HasSelector.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Spider\Mixins\Page;
+
+use Spider\Element;
+
+
+trait HasSelector {
+    private array $nodes = [];
+
+    public function find(string $selector): Element {
+        $FIRST_ELEMENT = 0;
+
+        $node = $this->xpath->query($selector)->item($FIRST_ELEMENT);
+
+        return new Element($node, $this->dom);
+    }
+
+    public function findAll(string $selector): self {
+        $nodes = $this->xpath->query($selector);
+    
+        foreach ($nodes as $node) {
+            $this->nodes[] = $node;
+        }
+
+        return $this;
+    }
+}
