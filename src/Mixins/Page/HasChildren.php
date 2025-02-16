b4b824e0be5faf7fcc2e493101edfd5645e01d93
ArefShojaei <arefshojaei82dev@gmail.com>
2025-02-16 17:08:15 +0330

    ADD: add eq method to get node element by index

--- src/Mixins/Page/HasChildren.php
+++ src/Mixins/Page/HasChildren.php
@@ -2,9 +2,19 @@
 
 namespace Spider\Mixins\Page;
 
+use Spider\Element;
+
 
 trait HasChildren {
     public function children(): array {
         return $this->nodes;
+    }
+
+    public function eq(int $index): ?Element {
+        $node = $this->nodes[$index] ?? null;
+        
+        if (!$node) return $node;
+
+        return new Element($node, $this->dom);
     }
 }
