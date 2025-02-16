65306cd9298f3257cba6927a19ec2b5012af86ce
ArefShojaei <arefshojaei82dev@gmail.com>
2025-02-16 15:51:13 +0330

    UPDATE: update to use traits and interfaces

--- src/Page.php
+++ src/Page.php
@@ -2,40 +2,27 @@
 
 namespace Spider;
 
-use Closure;
 use DOMDocument;
 use DOMXPath;
 use Spider\Interfaces\PageInterface;
+use Spider\Mixins\Page\{
+    HasChildren,
+    HasIterable,
+    HasSelector
+};
 
 
 final class Page implements PageInterface {
+    use HasSelector, HasIterable, HasChildren;
+
+
     private DOMDocument $dom;
 
     private DOMXPath $xpath;
-
-    private array $nodes = [];
 
 
     public function __construct(DOMDocument $dom, DOMXPath $xpath) {
         $this->dom = $dom;
         $this->xpath = $xpath;
-    }
-
-    public function find(string $selector): Element {
-        $FIRST_ELEMENT = 0;
-
-        $node = $this->xpath->query($selector)->item($FIRST_ELEMENT);
-
-        return new Element($node, $this->dom);
-    }
-
-    public function findAll(string $selector): self {
-        $nodes = $this->xpath->query($selector);
-    
-        foreach ($nodes as $node) {
-            $this->nodes[] = $node;
-        }
-
-        return $this;
     }
 }
