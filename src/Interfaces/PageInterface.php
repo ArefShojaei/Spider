5f4c06d69604244b8c19b08d5593241c713f084a
ArefShojaei <arefshojaei82dev@gmail.com>
2025-02-16 15:50:28 +0330

    ADD: add page interface

--- src/Interfaces/PageInterface.php
+++ src/Interfaces/PageInterface.php
@@ -2,9 +2,29 @@
 
 namespace Spider\Interfaces;
 
+use Closure;
 use Spider\Element;
 
 
-interface PageInterface {
+interface HasSelectorInterface {
     public function find(string $selector): Element;
+    
+    public function findAll(string $selector): self;
 }
+
+interface HasIterableInterface {
+    public function each(Closure $callback): void;
+    
+    public function map(Closure $callback): array;
+    
+    public function filter(Closure $callback): array;
+}
+
+interface HasChildrenInterface {
+    public function children(): array;
+}
+
+interface PageInterface extends 
+    HasSelectorInterface, 
+    HasIterableInterface,
+    HasChildrenInterface {}
