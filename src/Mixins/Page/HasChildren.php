6bac55ef38d03e797abecd649928dee306076289
ArefShojaei <arefshojaei82dev@gmail.com>
2025-02-16 17:28:17 +0330

    ADD: add to get element position by state

--- src/Mixins/Page/HasChildren.php
+++ src/Mixins/Page/HasChildren.php
@@ -2,17 +2,55 @@
 
 namespace Spider\Mixins\Page;
 
+use DOMNode;
 use Spider\Element;
 
 
 trait HasChildren {
+    private const FIRST_ELEMENT_CHILD = "FIRST";
+    
+    private const LAST_ELEMENT_CHILD = "LAST";
+    
+    private const DEFAULT_ELEMENT_CHILD_WITH_INDEX = "DEFAULT";
+
+
     public function children(): array {
         return $this->nodes;
     }
 
     public function eq(int $index): ?Element {
-        $node = $this->nodes[$index] ?? null;
+        $node = $this->getNodePosition(self::DEFAULT_ELEMENT_CHILD_WITH_INDEX)($index);
         
+        return $this->setElementByPosition($node);
+    }
+
+    public function first(): ?Element {
+        $node = $this->getNodePosition(self::FIRST_ELEMENT_CHILD)();
+        
+        return $this->setElementByPosition($node);
+    }
+
+    public function last(): ?Element {
+        $node = $this->getNodePosition(self::LAST_ELEMENT_CHILD)();
+        
+        return $this->setElementByPosition($node);
+    }
+
+    private function getNodePosition(string $position): ?object {
+        return match ($position) {
+            self::FIRST_ELEMENT_CHILD => function() {
+                return current($this->nodes) ?? null; 
+            },
+            self::LAST_ELEMENT_CHILD => function() {
+                return end($this->nodes) ?? null; 
+            },
+            self::DEFAULT_ELEMENT_CHILD_WITH_INDEX => function(int $index) {
+                return $this->nodes[$index] ?? null;
+            },
+        };
+    }
+
+    private function setElementByPosition(DOMNode $node): Element {
         if (!$node) return $node;
 
         return new Element($node, $this->dom);
